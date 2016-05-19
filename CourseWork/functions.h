1933888eb56a35ecd9d987a00499a856e789967d
AkidSe <akidse@mail.ru>
2016-05-19 08:37:57 +0300

    [functions] добавлена функція обрізки строки

--- CourseWork/functions.h
+++ CourseWork/functions.h
@@ -3,3 +3,14 @@ bool FileExists(std::string fname)
 	const char *filename = fname.c_str();
   return _access(filename, 0) != -1;
 }
+
+void cut_str(std::string &str, std::string &buf, int from, int to)
+{
+	for(int i = 0; i < str.length(); i++)
+	{
+		if(i >= from && i <= to)
+		{
+			buf.push_back(str[i]);
+		}
+	}
+}
