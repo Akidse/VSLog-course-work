0bd09005db0990ee6340159ac6681f83f77154fb
AkidSe <akidse@mail.ru>
2016-05-20 04:55:25 +0300

    [functions] добавлена функція виводу файлів або папок

--- CourseWork/functions.h
+++ CourseWork/functions.h
@@ -4,6 +4,30 @@ bool FileExists(std::string fname)
   return _access(filename, 0) != -1;
 }
 
+void list(std::string path, bool only_folder)
+{
+	int k = 0;
+	std::wstring stemp = std::wstring(path.begin(), path.end());
+    LPCWSTR sw = stemp.c_str();
+	WIN32_FIND_DATAW wfd;
+    HANDLE const hFind = FindFirstFileW(sw, &wfd);
+    if (INVALID_HANDLE_VALUE != hFind)
+    {
+      do
+      {
+		  if(only_folder && k >1)
+		  {
+            if((wfd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) && only_folder)std::wcout  << &wfd.cFileName[0]  << std::endl;
+			
+		  }
+		  k++;
+      } while (NULL != FindNextFileW(hFind, &wfd));
+ 
+      FindClose(hFind);
+    }
+}
+
+
 void cut_str(std::string &str, std::string &buf, int from, int to)
 {
 	for(int i = 0; i < str.length(); i++)
